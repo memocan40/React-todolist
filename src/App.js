6a1a3c87bb10740a28d255ffa95f246a60c65e5c
memo40 <memocann4040@gmail.com>
2021-01-05 17:22:31 +0100

    addet todo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import {useState} from 'react';
 import './App.css';
-
+import Todo from "./Todo";
 function App() {
   const defaultTodos = ['First To Do', 'Second To Do', 'Third To Do', 'Fourth To Do', 'Fifth To Do', 'Sixth To Do']
   let [todos, setTodos] = useState(defaultTodos)
 
   return (
     <div className="App">
-      Hello World
+      Hello World<Todo/>
     </div>
   );
 }
---
+++ src/Todo/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+
+
+export default function Todo({todotitle}){
+
+
+
+    return(
+        <div className="todo">test{todotitle}</div>
+    )
+}
